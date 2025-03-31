e456214cdb307a04520cfe85783e3ccae284e33f
Rajesh Sitaraman <rajeshrd@amazon.com>
2025-03-31 13:55:09 -0700

    Update task in main.py to request flight details for a vacation trip

--- main.py
+++ main.py
@@ -14,7 +14,15 @@ llm = ChatBedrock(
 
 async def main():
     agent = Agent(
-        task="Get the 2025 holiday schedule for LAUSD.",
+        task="""
+        1. Find me cheapeast flight from San Francisco to Chennai with shortest duration for my vacation.
+        2. I want to travel in last week of May and return in June 3rd week, any day is fine.
+        3. Economy or Economy plus is fine.
+        4. Use google.com/flights to find the flight.
+        5. Provide me the top 3 flights with the details like price, duration, and departure time.
+        6. Format result in a table with columns: Flight, Price, Duration, Departure Time, Return date.
+        7. Provide me the source link to book the flight.
+        """,
         llm=llm,
     )
     await agent.run()
